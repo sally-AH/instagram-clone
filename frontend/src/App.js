0137e6118a30c89d4630076510882d331524a292
sally-AH <sallyabdalhafeez@gmail.com>
2023-08-09 17:00:40 +0300

    Frontend <> Update src Folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React  from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Authenication from './pages/Authentication';
+import Profile from './pages/profile';
+import Landing from './pages/landing';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element= {<Landing/>} />
+          <Route path="/profile" element= {<Profile/>} />
+          <Route path="/authenication" element= {<Authenication/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/Authentication/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authenication = () => {
+  return (
+    <div>
+      <h1>Authenication Page</h1>
+    </div>
+  )
+}
+
+export default Authenication
